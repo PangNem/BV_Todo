60efcd85401d8c0d57efe521b3abf4b2f837e2ed
PangNem <sprnd645@gmail.com>
2020-05-19 00:55:35 +0900

    Add initTodo, createTodo

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <b-card
-    header="aboout"
+    header="오늘 할 일"
     style="max-width: 40rem;
     margin: auto;
     margin-top: 10vh;"
@@ -13,10 +13,10 @@
         <b-container fluid>
           <b-row class="my-1">
             <b-col sm="10">
-              <b-form-input v-model="title" type="text" placeholder="새 할 일을 적으세요." />
+              <b-form-input v-model="newToDoItem.title" type="text" placeholder="새 할 일을 적으세요." v-on:keyup.enter="createToDo" />
             </b-col>
             <b-col sm="2">
-              <b-button variant="outline-primary">추가</b-button>
+              <b-button variant="outline-primary" v-on:click="createToDo">추가</b-button>
             </b-col>
           </b-row>
         </b-container>
@@ -39,24 +39,42 @@
 
 <script>
 const axios = require('axios')
+const BASE_URL = 'http://localhost:3000/todo/'
 
 export default {
   name: 'hello',
   data: () => {
     return {
       toDoItems: [],
-      data: []
+      newToDoItem: {}
+    }
+  },
+  methods: {
+    initToDo: function () {
+      axios.get(BASE_URL)
+        .then(response => {
+          this.toDoItems = response.data.data
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    },
+    createToDo: function (event) {
+      event.preventDefault()
+      if (!this.newToDoItem.title) return
+      axios.post(BASE_URL, this.newToDoItem)
+        .then(response => {
+          console.log(response)
+          this.initToDo()
+          this.newToDoItem = {}
+        })
+        .catch(error => {
+          console.error(error)
+        })
     }
   },
   created () {
-    axios
-      .get('http://localhost:3000/todo/')
-      .then((response) => {
-        this.toDoItems = response.data.data
-      })
-      .catch((e) => {
-        console.log('error : ', e)
-      })
+    this.initToDo()
   }
 
 }
